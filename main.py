2237221d8d7226914f7b38a2eaaa28360f35c5ed
AdamDemian <adamdemian1@gmail.com>
2021-03-07 21:00:39 +0100

    sorry fraze added

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as sourse:
-    print("Povec niečo")
-    audio = r.listen(sourse)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+
+def record_audio():
+    with sr.Microphone() as sourse:
+        print("Povec niečo")
+        audio = r.listen(sourse)
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print("Prepáč nerozumela som.")
+        except sr.RequestError:
+            print("Sorry, my speach server is down.")
