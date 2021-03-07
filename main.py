94a66e80817be2dae2c555ac5800a08b5344d684
AdamDemian <adamdemian1@gmail.com>
2021-03-07 21:18:32 +0100

    she can tell time and her name

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import speech_recognition as sr
+from time import ctime
 
 r = sr.Recognizer()
 
 
 def record_audio():
     with sr.Microphone() as sourse:
-        print("Povec niečo")
         audio = r.listen(sourse)
+        voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            print(voice_data)
         except sr.UnknownValueError:
             print("Prepáč nerozumela som.")
         except sr.RequestError:
             print("Sorry, my speach server is down.")
+        return voice_data
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print("My name is Alice")
+    if 'what time is it' in voice_data:
+        print(ctime())
+
+
+print("Ako ti môžem pomôcť?")
+voice_data = record_audio()
+print(voice_data)
+respond(voice_data)
